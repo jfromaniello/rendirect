a31fa6fc343c7e4387038add5e759269b3061a53
José F. Romaniello <jfromaniello@gmail.com>
2013-04-23 15:55:50 -0300

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var ejs = require('ejs');
+var fs = require('fs');
+var defaultTemplate = ejs.compile(fs.readFileSync(__dirname + '/defaultTemplate.ejs').toString());
+
+module.exports = function (options) {
+  return function (req, res, next) {
+    req.rendirect = function (rendirOptions) {
+      
+      if (!rendirOptions.url) {
+        throw new Error('rendirect: url is required');
+      }
+
+      rendirOptions.wait = rendirOptions.wait || 5;
+      rendirOptions.message = rendirOptions.message || ('Redirecting to ' + rendirOptions.url);
+
+      if (options && options.view){
+        return res.render(options.view, rendirOptions);
+      }
+
+      res.set('Content-Type', 'text/html');
+      res.send(defaultTemplate(rendirOptions));
+    };
+    next();
+  };
+};
---
+++ test/main.tests.js
@@ -0,0 +1,96 @@
+var expect    = require('chai').expect;
+var jsdom     = require('jsdom');
+var rendirect = require('../');
+
+describe('rendirects!', function () {
+  it('should add the rendirect function to the request object', function () {
+    var req = {};
+    
+    rendirect()(req, {}, function(){});
+    
+    expect(req.rendirect)
+      .to.be.a('function');
+  });
+
+  it('should call next', function (done) {
+    rendirect()({}, {}, done);
+  });
+
+  describe('when rendirecting', function () {
+    var headers, content;
+
+    beforeEach(function () {
+      headers={};
+      content='';
+      
+      var req = {};
+      
+      var res = {
+        set: function (k, h) {
+          headers[k] = h;
+        },
+        send: function (c) {
+          content = c;
+        } 
+      };
+
+      rendirect()(req, res, function(){});
+
+      req.rendirect({ wait: 5, message: 'Jose is redirecting you', url: 'http://google.com'});
+    });
+
+    it('should put the Content-Type', function () {
+      expect(headers['Content-Type']).to.equal('text/html');
+    });
+
+    it('should contain the meta tag', function () {
+      jsdom.env(
+        content,
+        ["http://code.jquery.com/jquery.js"],
+        function (errors, window) {
+          var meta = window.$('meta')[0];
+          expect(meta.attr('http-equiv')).to.equal('refresh');
+          expect(meta.attr('url').split(';')[0]).to.equal('5');
+          expect(meta.attr('url').split(';')[1]).to.equal('URL=http://google.com');
+        }
+      );
+    });
+
+    it('should contain the message', function () {
+      expect(content.indexOf('Jose is redirecting you')).to.not.equal(-1);
+    });
+
+  }); 
+
+
+  describe('when using custom view', function () {
+    var viewName, locals;
+
+    beforeEach(function () {
+      viewName={};
+      locals='';
+      
+      var req = {};
+      
+      var res = {
+        render: function (vn, ls) {
+          locals = ls;
+          viewName = vn;
+        } 
+      };
+
+      rendirect({view: 'myrendirect'})(req, res, function(){});
+
+      req.rendirect({ wait: 5, message: 'Jose is redirecting you', url: 'http://google.com'});
+    });
+
+    it('should work', function () {
+      expect(viewName).to.equal('myrendirect');
+      expect(locals.wait).to.equal(5);
+      expect(locals.message).to.equal('Jose is redirecting you');
+      expect(locals.url).to.equal('http://google.com');
+    });
+
+  }); 
+
+});
