2caa2b09e033bfed5a4bbfa8cbc3d48b6ed82815
José F. Romaniello <jfromaniello@gmail.com>
2014-12-30 16:43:56 -0300

    fixed tests

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var defaultTemplate = ejs.compile(fs.readFileSync(__dirname + '/defaultTemplate.
 module.exports = function (options) {
   return function (req, res, next) {
     res.rendirect = function (rendirOptions) {
-      
+
       if (!rendirOptions.url) {
         throw new Error('rendirect: url is required');
       }
 
-      rendirOptions.wait = rendirOptions.wait || 5;
+      rendirOptions.wait = typeof rendirOptions.wait === 'undefined' ? 5 : rendirOptions.wait;
       rendirOptions.message = rendirOptions.message || ('Redirecting to ' + rendirOptions.url);
 
       if (options && options.view){
--- test/main.tests.js
+++ test/main.tests.js
@@ -5,9 +5,9 @@ var rendirect = require('../');
 describe('rendirects!', function () {
   it('should add the rendirect function to the request object', function () {
     var res = {};
-    
+
     rendirect()({}, res, function(){});
-    
+
     expect(res.rendirect)
       .to.be.a('function');
   });
@@ -22,16 +22,16 @@ describe('rendirects!', function () {
     beforeEach(function () {
       headers={};
       content='';
-      
+
       var req = {};
-      
+
       var res = {
         set: function (k, h) {
           headers[k] = h;
         },
         send: function (c) {
           content = c;
-        } 
+        }
       };
 
       rendirect()(req, res, function(){});
@@ -43,15 +43,16 @@ describe('rendirects!', function () {
       expect(headers['Content-Type']).to.equal('text/html');
     });
 
-    it('should contain the meta tag', function () {
+    it('should contain the meta tag', function (done) {
       jsdom.env(
         content,
-        ["http://code.jquery.com/jquery.js"],
+        [ "http://code.jquery.com/jquery.js" ],
         function (errors, window) {
-          var meta = window.$('meta')[0];
+          var meta = window.$('meta');
           expect(meta.attr('http-equiv')).to.equal('refresh');
-          expect(meta.attr('url').split(';')[0]).to.equal('5');
-          expect(meta.attr('url').split(';')[1]).to.equal('URL=http://google.com');
+          expect(meta.attr('content').split(';')[0]).to.equal('5');
+          expect(meta.attr('content').split(';')[1]).to.equal("URL='http://google.com'");
+          done();
         }
       );
     });
@@ -60,7 +61,46 @@ describe('rendirects!', function () {
       expect(content.indexOf('Jose is redirecting you')).to.not.equal(-1);
     });
 
-  }); 
+  });
+
+  describe('when rendirecting with no wait', function () {
+    var headers, content;
+
+    beforeEach(function () {
+      headers={};
+      content='';
+
+      var req = {};
+
+      var res = {
+        set: function (k, h) {
+          headers[k] = h;
+        },
+        send: function (c) {
+          content = c;
+        }
+      };
+
+      rendirect()(req, res, function(){});
+
+      res.rendirect({ wait: 0, message: 'Jose is redirecting you', url: 'http://google.com'});
+    });
+
+    it('should contain the meta tag', function (done) {
+      jsdom.env(
+        content,
+        [ "http://code.jquery.com/jquery.js" ],
+        function (errors, window) {
+          var meta = window.$('meta');
+          expect(window.$('p').length).to.equal(0);
+          expect(meta.attr('http-equiv')).to.equal('refresh');
+          expect(meta.attr('content').split(';')[0]).to.equal('0');
+          expect(meta.attr('content').split(';')[1]).to.equal("URL='http://google.com'");
+          done();
+        }
+      );
+    });
+  });
 
 
   describe('when using custom view', function () {
@@ -69,14 +109,14 @@ describe('rendirects!', function () {
     beforeEach(function () {
       viewName={};
       locals='';
-      
+
       var req = {};
-      
+
       var res = {
         render: function (vn, ls) {
           locals = ls;
           viewName = vn;
-        } 
+        }
       };
 
       rendirect({view: 'myrendirect'})(req, res, function(){});
@@ -91,6 +131,6 @@ describe('rendirects!', function () {
       expect(locals.url).to.equal('http://google.com');
     });
 
-  }); 
+  });
 
 });
